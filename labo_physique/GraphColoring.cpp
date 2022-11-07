140f12cea947d3e8102df82517e92022419ad1f2
Santiago Chiappe <47071792+SantiagoChiappe@users.noreply.github.com>
2022-11-07 14:21:52 -0500

    Coloring works

--- labo_physique/GraphColoring.cpp
+++ labo_physique/GraphColoring.cpp
@@ -9,6 +9,7 @@ void GraphColoring::color(ParticleSystem& particleSystem)
     ColorList C;
 
     std::vector<Particle>& particles = particleSystem.getParticles();
+    std::vector<Spring>& springs = particleSystem.getSprings();
 
     // Initialiser toutes les particules avec color = -1 
     for (Particle& p : particles)
@@ -19,9 +20,43 @@ void GraphColoring::color(ParticleSystem& particleSystem)
     // TODO Calculer les couleurs de toutes les particules du système. 
     //      Boucler sur chaque particule et appeler la fonction findColor.
 
+    for (Particle& p : particles) 
+    {
+        p.color = this->findColor(p, particles, springs, C);
+    }
+
     // TODO Construire les partitions qui correspond à chaque couleur. 
     //     Les partitions sont représentées par un tableau d'indices de particules, un pour chaque couleur. 
     //     Stocker les partitions dans m_partitions.
+    /*m_partitions.resize(C.size());
+    int i = 0;
+    for (Particle& p : particles)
+    {
+        auto partition = m_partitions[p.color];
+        partition.push_back(i);
+    }
+
+    Vector<int, Dynamic> count(C.size());
+    for (Particle& p : particles)
+    {
+        count(p.color)++;
+    }
+
+    m_partitions.resize(C.size());
+    int i = 0;
+    for (auto partition : m_partitions) 
+    {
+        partition.resize(count(i));
+        i++;
+    }
+
+    i = 0;
+    for (Particle& p : particles)
+    {
+        auto partition = m_partitions[p.color];
+        partition.push_back(i);
+        count(p.color)++;
+    }*/
 }
 
 int GraphColoring::findColor(const Particle& p, const std::vector<Particle>& particles, const std::vector<Spring>& springs, ColorList& C) const
@@ -29,8 +64,25 @@ int GraphColoring::findColor(const Particle& p, const std::vector<Particle>& par
     // TODO Trouver la première couleur de la palette C qui n'est pas attribuée à une particule voisine. 
     //      Si une couleur est introuvable, ajouter une nouvelle couleur à la palette et retournez la couleur. 
     //      Utiliser la fonction findNeighbors pour assembler une liste de particules qui sont directement connectées à la particule p par un ressort (les voisines).
+    int nbColors = C.size() > 0 ? C.size() : 1;
+    Vector<int, Dynamic> count(nbColors);
+    for (auto y : findNeighbors(p, particles, springs)) 
+    {
+        if(y->color >= 0) count(y->color)++;
+    }
 
-    return -1;
+    for (int c = 0; c < nbColors; c++) 
+    {
+        if (count(c) == 0) 
+        {
+            if (C.size() == 0) C.push_back(c);
+            return c;
+        }
+    }
+
+    int c = nbColors;
+    C.push_back(c);
+    return c;
 }
 
 NeighborList GraphColoring::findNeighbors(const Particle& p, const std::vector<Particle>& particles, const std::vector<Spring>& springs) const
